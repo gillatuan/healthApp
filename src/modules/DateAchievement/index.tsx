77d6fdd8e6f54759b2624bc29b2e31014c40e76f
gillatuan <tuan.buidoanngoc@gmail.com>
2022-12-01 13:05:25 +0700

    implement DateAchievement

---
+++ src/modules/DateAchievement/index.tsx
@@ -0,0 +1,36 @@
+import { Figure } from "react-bootstrap"
+
+import "modules/DateAchievement/index.scss"
+
+type TDATEACHIVEMENT = {
+  datetime: string
+  image: string
+  percentage: string
+}
+export const DateAchievement = (props: TDATEACHIVEMENT) => {
+  const { datetime, image, percentage } = props
+
+  return (
+    <div className="date-achievement">
+      <Figure>
+        <Figure.Image height="100%" alt="171x180" src={image} />
+        <div className="caption-container">
+          <Figure.Caption className="datetime">
+            <span>{datetime}</span>
+          </Figure.Caption>
+          <Figure.Caption className="percentage">
+            <span>{percentage}</span>
+          </Figure.Caption>
+        </div>
+      </Figure>
+      <div className="set-size charts-container">
+        <div className="pie-wrapper progress-75">
+          <div className="pie">
+            <div className="left-side half-circle"></div>
+            <div className="right-side half-circle"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
